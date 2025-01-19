e0fdab882740feb04abd734c31c25d79207d1aa3
Carlos Avila <cavilart21@gmail.com>
2025-01-19 17:21:49 +0100

    adding template, setting up model and wire up the view

--- toursagency/models.py
+++ toursagency/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return (f"| ID: {self.id} | ORIGIN: {self.origin_country} | DESTINATION: {self.destination_country} | NUMBER OF NIGHTS: {self.number_of_nights} | PRICE: {self.number_of_nights} ")
--- toursagency/views.py
+++ toursagency/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from .models import Tour
 
 def index(request):
-    return HttpResponse("Hello to tours")
+    tours = Tour.objects.all()
+    context = {'tours': tours}
+
+    return render(request, 'index.html', context)
